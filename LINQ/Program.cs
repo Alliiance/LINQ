2ca2f2de23f01846a67e42f493693fd0bc233a34
Maksim <maksim2423@yandex.ru>
2019-06-12 12:07:28 +0300

    New

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -36,7 +36,6 @@ namespace LINQ
         static void Main(string[] args)
         {
 
-
             /*  Level 1  */
 
             // 1. Print all numbers from 10 to 50 separated by commas
@@ -52,17 +51,24 @@ namespace LINQ
             // Console.WriteLine(String.Join(",", "aaa;abb;ccc;dap".Split(';').Where(f => f.Contains("a"))));
 
             //5. Output number of letters 'a' in the words with this letter in string "aaa;abb;ccc;dap" separated by comma
-            //Console.WriteLine(String.Join(",", "aaa;abb;ccc;dap".Split(';').Where(f => f == "a").Count())); ?????????
-            //Console.WriteLine(String.Join(",", "aaa;abb;ccc;dap".Split(';').Count(f => f.Contains("a"))));  ?????????
+            //Console.WriteLine(String.Join(",", "aaa;abb;ccc;dap".Split(';').Count(f => f.Contains("a"))));  ?????
 
             //6. Output true if word "abb" exists in line  "aaa;xabbx;abb;ccc;dap", otherwise false
             //Console.WriteLine(String.Join(",", "aaa;xabbx;abb;ccc;dap").Split(';').Contains("abb"));
 
             //7. Get the longest word in string "aaa;xabbx;abb;ccc;dap"
-            //Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Max(f => f.Length)); ????
-            //Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Where(k => k.Length == "aaa;xabbx;abb;ccc;dap".Split(';').Max(f => f.Length))); ????
+            //Console.WriteLine("aaa;xabbx;abb;ccc;dSADap".Split(';').First(item => item.Length == "aaa;xabbx;abb;ccc;dSADap".Split(';').Max(f => f.Length)));
 
             //8. Calculate average length of word in string "aaa;xabbx;abb;ccc;dap"
+            //Console.WriteLine(String.Join(",", "aaa;xabbx;abb;SDb;ccc;dap").Split(';').Average(f => f.Length));
+
+            //9. Print the shortest word reversed in string "aaa;xabbx;abb;ccc;dap;zh"
+            //Console.WriteLine("baaa;aabb;xabbx;abb;ccc;dap;zh".Split(';').First(item => item.Length == "baaa;aabb;xabbx;abb;ccc;dap;zh".Split(';').Min(f => f.Length)));
+
+            //10. Print true if in the first word that starts from "aa" all letters are 'a' otherwise false "baaa;aabb;xabbx;abb;ccc;dap;zh"
+            //Console.WriteLine("baaa;aabb;xabbx;abb;ccc;dap;zh".Split(';'). ????
+
+            //11. Print last word in sequence that excepting first two elements that ends with "bb"
 
 
 
@@ -86,21 +92,42 @@ namespace LINQ
                         "Leonardo DiCaprio"
                     };
 
+            //  Console.WriteLine(string.Join(Environment.NewLine,
 
             //1. Output all elements excepting ArtObjects
-            //var element = data.OfType<Book>().GroupBy(f => f.Author).ToDictionary(f => f.Key , f => f.ToList());
-            //foreach (var item in element)
-            //        Console.WriteLine(item);
-
+            //Console.WriteLine(string.Join(Environment.NewLine, data.Except(data.OfType<ArtObject>())));
 
             //2. Output all actors names
-            //var element = data.OfType<Book>().GroupBy(f => f.Author);
-            //foreach (var name in element)
-            //    Console.WriteLine(name.Key);
-            //2. Output all actors names
+            //Console.WriteLine(string.Join(Environment.NewLine,data.OfType<Film>().SelectMany(f => f.Actors).Select(f => f.Name)));
 
+            //3. Output number of actors born in august
+            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Film>().SelectMany(f => f.Actors).Where(f => f.Birthdate.Month == 8).Count()));
 
-            Console.WriteLine(String.Join(",", data.OfType<Actor>())); 
+            //4. Output two oldest actors names
+            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Film>().SelectMany(f => f.Actors).OrderBy(f => f.Birthdate).Select(f => f.Name).Take(2)));
+
+            //5. Output number of books per authors
+            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Book>().GroupBy(f => f.Author).SelectMany(f => f.Key + " : " + f.Count())));
+
+            //6. Output number of books per authors and films per director
+            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<ArtObject>().GroupBy(f => f.Author).Select(f => f.Key + " : " + f.Count())));
+
+            //7. Output how many different letters used in all actors names
+            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Film>().SelectMany(f => f.Actors).Select(f => f.Name).Distinct().Select(f => f.Distinct().Count())));
+
+            //8. Output names of all books ordered by names of their authors and number of pages
+            //Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Book>().OrderBy(f => f.Author).OrderBy(f => f.Pages).Select(f => f.Name)));
+
+            //9. Output actor name and all films with this actor
+            /////////////// 
+
+            //10. Output sum of total number of pages in all books and all int values inside all sequences in data
+            Console.WriteLine(string.Join(Environment.NewLine, data.OfType<Book>().Select(f => f.Pages).Sum() + " " + data.OfType<IEnumerable<int>>().SelectMany(f => f.OfType<int>()).Sum()));
+
+            //11. Get the dictionary with the key - book author, value - list of author's books
+            ////////////////////////////   
+
+            //12. Output all films of "Matt Damon" excluding films with actors whose name are presented in data as strings
 
             Console.ReadKey();
         }
